990bfa3d2c1c528ad65397142d0e216936d23ccc
Rattapoom P. <celica2548@gmail.com>
2026-08-09 14:25:37 +0700

    Add hero banner

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "mysite",
-    "restaurants.apps.RestaurantsConfig",
+    "restaurants",
 ]
 
 MIDDLEWARE = [
---
+++ restaurants/templatetags/card_image.py
@@ -0,0 +1,23 @@
+from django import template
+from django.templatetags.static import static
+
+register = template.Library()
+
+
+def _get_card_image_context(default_path: str, image, alt="", aspect_ratio="1 / 1"):
+    return {
+        "image": image,
+        "alt": alt,
+        "default_image": static(default_path),
+        "aspect_ratio": aspect_ratio,
+    }
+
+
+@register.inclusion_tag("includes/card_image.html")
+def restaurant_card_image(image, alt=""):
+    return _get_card_image_context("images/restaurant.jpg", image, f"{alt} Restaurant")
+
+
+@register.inclusion_tag("includes/card_image.html")
+def menu_card_image(image, alt=""):
+    return _get_card_image_context("images/food.jpg", image, alt, "16/9")
